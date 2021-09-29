813e898486e725ca3d4b285c5c273271f13b3200
Jadamka <59932297+Jadamka@users.noreply.github.com>
2021-09-29 19:46:29 +0200

    Add files via upload

---
+++ ImageViewer/ImageViewer/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing; //Nuget package nainstalovat System.Drawing.Common kvuli Image a Graphics
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ImageViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(180, 40);
+
+            Console.WriteLine("Enter directory of image you wanna display: ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Graphics in console window!");
+
+            Point location = new Point(10, 10);
+            Size imageSize = new Size(50, 20);//image size in characters
+
+            // draw some placeholders
+            Console.SetCursorPosition(location.X - 1, location.Y);
+            Console.Write(">");
+            Console.SetCursorPosition(location.X + imageSize.Width, location.Y);
+            Console.Write("<");
+            Console.SetCursorPosition(location.X - 1, location.Y + imageSize.Height - 1);
+            Console.Write(">");
+            Console.SetCursorPosition(location.X + imageSize.Width, location.Y + imageSize.Height - 1);
+            Console.WriteLine("<");
+
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonPictures), address);
+            using(Graphics g = Graphics.FromHwnd(GetConsoleWindow()))
+            {
+                using (Image image = Image.FromFile(path))
+                {
+                    Size fontSize = GetConsoleFontSize();
+
+                    Rectangle imageRect = new Rectangle(location.X * fontSize.Width, location.Y * fontSize.Height,
+                        imageSize.Width * fontSize.Width, imageSize.Height * fontSize.Height);
+                    g.DrawImage(image, imageRect);
+                }
+            }
+        }
+
+        private static Size GetConsoleFontSize()
+        {
+            IntPtr outHandle = CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE,
+                                            FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                            IntPtr.Zero,
+                                            OPEN_EXISTING,
+                                            0,
+                                            IntPtr.Zero);
+
+            int errorCode = Marshal.GetLastWin32Error();
+            if (outHandle.ToInt32() == INVALID_HANDLE_VALUE)
+            {
+                throw new IOException("Unable to open CONOUT$", errorCode);
+            }
+
+            ConsoleFontInfo cfi = new ConsoleFontInfo();
+            if (!GetCurrentConsoleFont(outHandle, false, cfi))
+            {
+                throw new InvalidOperationException("Unable to get font information.");
+            }
+
+            return new Size(cfi.dwFontSize.X, cfi.dwFontSize.Y);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode, 
+            IntPtr lpSecurityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool GetCurrentConsoleFont(IntPtr hConsoleOutput, bool bMaximumWindow,
+            [Out][MarshalAs(UnmanagedType.LPStruct)] ConsoleFontInfo lpConsoleCurrentFont);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal class ConsoleFontInfo
+        {
+            internal int nFont;
+            internal Coord dwFontSize;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct Coord
+        {
+            [FieldOffset(0)]
+            internal short X;
+            [FieldOffset(2)]
+            internal short Y;
+        }
+
+        private const int GENERIC_READ = unchecked((int)0x80000000);
+        private const int GENERIC_WRITE = 0x40000000;
+        private const int FILE_SHARE_READ = 1;
+        private const int FILE_SHARE_WRITE = 2;
+        private const int INVALID_HANDLE_VALUE = -1;
+        private const int OPEN_EXISTING = 3;
+    }
+}
